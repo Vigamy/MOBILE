58f49784ef0dad90d5a999f651bc50fd57006e22
Vigamy <andreottiwill@gmail.com>
2024-03-09 14:41:07 -0300

    Ajeitando alguns ajustes do jogo da velha e de estudos

--- JogoDaVelha/app/src/main/java/tottaly/not/jogodavelha/MainActivity.java
+++ JogoDaVelha/app/src/main/java/tottaly/not/jogodavelha/MainActivity.java
@@ -38,37 +38,29 @@ public class MainActivity extends AppCompatActivity {
         if (!stop){
 
             //Jogador adicionar X
-            if (bt.getContentDescription() == null || bt.getContentDescription() == "") {
-                if (xo) {
-                    bt.setImageResource(R.drawable.x);
-                    bt.setContentDescription("X");
-                } //else {
-//                    bt.setImageResource(R.drawable.o);
-//                    bt.setContentDescription("O");
-//                }
-
-                xo = !xo;
-                rodada++;
-            }
+            playerAdicionar(bt);
 
             //Verificar se o jogo acabou
-            if (rodada >= 5) {
-                if (acharVencedor()) {
-                    Toast.makeText(this, "FIM DE JOGO", Toast.LENGTH_SHORT).show();
+            if (rodada >= 5 && rodada < 9) {
+                if (acharVencedor() == 1) {
+                    Toast.makeText(this, "FIM DE JOGO\nX VENCEU", Toast.LENGTH_SHORT).show();
+                    stop = true;
+                }else if (acharVencedor() == 0){
+                    Toast.makeText(this, "FIM DE JOGO\nO VENCEU", Toast.LENGTH_SHORT).show();
                     stop = true;
                 }
-            } else if (rodada == 9) {
+            } else if (rodada == 9 || acharVencedor() == -1) {
                 Toast.makeText(this, "EMPATE", Toast.LENGTH_SHORT).show();
             }
             
             //SLEEP
-            try {
-                // Pausa a execução por 1 segundos (1000 milissegundos)
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                // Lidar com a exceção, se ocorrer
-                e.printStackTrace();
-            }
+//            try {
+//                // Pausa a execução por 1 segundos (1000 milissegundos)
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                // Lidar com a exceção, se ocorrer
+//                e.printStackTrace();
+//            }
 
             //Colocar aleatório
             while (true){
@@ -84,53 +76,101 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //Verificar se o jogo acabou
-            if (rodada >= 5) {
-                if (acharVencedor()) {
-                    Toast.makeText(this, "FIM DE JOGO", Toast.LENGTH_SHORT).show();
+            if (rodada >= 5 && rodada < 9) {
+                if (acharVencedor() == 1) {
+                    Toast.makeText(this, "FIM DE JOGO\nX VENCEU", Toast.LENGTH_SHORT).show();
+                    stop = true;
+                }else if (acharVencedor() == 0){
+                    Toast.makeText(this, "FIM DE JOGO\nO VENCEU", Toast.LENGTH_SHORT).show();
                     stop = true;
                 }
-            } else if (rodada == 9) {
+            } else if (rodada == 9 || acharVencedor() == -1) {
                 Toast.makeText(this, "EMPATE", Toast.LENGTH_SHORT).show();
             }
 
         }
 
     }
-    public boolean acharVencedor(){
-        // Coluna 1
+    public void playerAdicionar(ImageView bt){
+        if (bt.getContentDescription() == null || bt.getContentDescription() == "") {
+            if (xo) {
+                bt.setImageResource(R.drawable.x);
+                bt.setContentDescription("X");
+            } //else {
+//                    bt.setImageResource(R.drawable.o);
+//                    bt.setContentDescription("O");
+//                }
 
+            xo = !xo;
+            rodada++;
+        }
+    }
+    public int acharVencedor(){
+        // Coluna 1
         if(casa[0].getContentDescription() == casa[3].getContentDescription() && casa[3].getContentDescription() == casa[6].getContentDescription()){
-           return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Coluna 2
         else if(casa[1].getContentDescription() == casa[4].getContentDescription() && casa[4].getContentDescription() == casa[7].getContentDescription()){
-                return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Coluna 3
         else if(casa[2].getContentDescription() == casa[5].getContentDescription() && casa[5].getContentDescription() == casa[8].getContentDescription()){
-                return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Linha 1
         else if(casa[0].getContentDescription() == casa[1].getContentDescription() && casa[1].getContentDescription() == casa[2].getContentDescription()) {
-                return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Linha 2
         else if(casa[3].getContentDescription() == casa[4].getContentDescription() && casa[4].getContentDescription() == casa[5].getContentDescription()) {
-                return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Linha 3
         else if(casa[6].getContentDescription() == casa[7].getContentDescription() && casa[7].getContentDescription() == casa[8].getContentDescription()) {
-            return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Diagonal 1
         else if(casa[0].getContentDescription() == casa[4].getContentDescription() && casa[4].getContentDescription() == casa[8].getContentDescription()) {
-            return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
         // Diagonal 2
         else if(casa[2].getContentDescription() == casa[4].getContentDescription() && casa[4].getContentDescription() == casa[6].getContentDescription()) {
-            return true;
+            if(casa[0].getContentDescription() == "X"){
+                return 1;
+            }else{
+                return 0;
+            }
         }
-        return false;
+        return -1;
     }
     public void resetar(View view){
         recreate();
